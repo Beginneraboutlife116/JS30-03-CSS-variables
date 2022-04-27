255774f20dd463b7a9014a5c52e52b5dace41a8e
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-04-27 20:07:35 +0800

    switch css to scss and make random color for cube side(bad commit)

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const wrapper = document.querySelector('.wrapper')
+const interface = document.querySelector('.wrapper')
 const cube = document.querySelector('.cube')
 const cubeSides = document.querySelectorAll('.cube__side')
-let x = 0
-let y = 0
-let horizontalHalfValue = wrapper.offsetWidth / 2
-let verticalHalfValue = wrapper.offsetHeight / 2
-let isDragging = false
+const cubeSidesColors = []
+let forRotateX = 0
+let forRotateY = 0
+let horizontalHalfValue = interface.clientWidth / 2
+let verticalHalfValue = interface.clientHeight / 2
+let isGrabbing = false
 
 function calculateRotateX(inputY) {
   let rotateXDeg = (verticalHalfValue - inputY) / verticalHalfValue * 90
@@ -15,32 +16,52 @@ function calculateRotateY(inputX) {
   let rotateYDeg = (inputX - horizontalHalfValue) / horizontalHalfValue * 90
   return `${rotateYDeg}deg`
 }
+function setSideColor(side, red, green, blue, alpha) {
+  if (!side) return
+  side.style.setProperty('--red', red)
+  side.style.setProperty('--green', green)
+  side.style.setProperty('--blue', blue)
+  side.style.setProperty('--alpha', `${alpha}%`)
+}
 
-wrapper.addEventListener('mousedown', (e) => {
-  if ([...cubeSides].includes(e.target)) return
-  x = e.offsetX
-  y = e.offsetY
-  cube.style.setProperty('--rotate-x-deg', calculateRotateX(y))
-  cube.style.setProperty('--rotate-y-deg', calculateRotateY(x))
-  isDragging = true
-  wrapper.style.setProperty('--pointer-style', 'grabbing')
+cubeSides.forEach(cubeSide => {
+  const values = []
+  for (let i = 0; i < 4; i++) {
+    if (i === 3) {
+      values.push(Math.floor(Math.random() * 50) + 50)
+    } else {
+      values.push(Math.floor(Math.random() * 256))
+    }
+  }
+  setSideColor(cubeSide, ...values)
+  cubeSidesColors.push(values)
 })
+console.log(cubeSidesColors)
 
-wrapper.addEventListener('mousemove', (e) => {
-  if (isDragging) {
-    x = e.offsetX
-    y = e.offsetY
-    cube.style.setProperty('--rotate-x-deg', calculateRotateX(y))
-    cube.style.setProperty('--rotate-y-deg', calculateRotateY(x))
+interface.addEventListener('mousedown', (e) => {
+  if (e.target !== interface) return
+  forRotateY = e.clientX
+  forRotateX = e.clientY
+  cube.style.setProperty('--rotate-x-deg', calculateRotateX(forRotateX))
+  cube.style.setProperty('--rotate-y-deg', calculateRotateY(forRotateY))
+  interface.style.setProperty('--pointer-style', 'grabbing')
+  isGrabbing = true
+})
+interface.addEventListener('mousemove', (e) => {
+  if (isGrabbing) {
+    forRotateY = e.clientX
+    forRotateX = e.clientY
+    cube.style.setProperty('--rotate-x-deg', calculateRotateX(forRotateX))
+    cube.style.setProperty('--rotate-y-deg', calculateRotateY(forRotateY))
+    cube.style.setProperty('--cube-pointer', 'grabbing')
   }
 })
-
-wrapper.addEventListener('mouseup', () => {
-  isDragging = false
-  wrapper.style.setProperty('--pointer-style', '')
+interface.addEventListener('mouseup', () => {
+  isGrabbing = false
+  interface.style.setProperty('--pointer-style', '')
+  cube.style.setProperty('--cube-pointer', '')
 })
-
 window.addEventListener('resize', () => {
-  horizontalHalfValue = wrapper.offsetWidth / 2
-  verticalHalfValue = wrapper.offsetHeight / 2
+  horizontalHalfValue = interface.clientWidth / 2
+  verticalHalfValue = interface.clientHeight / 2
 })
